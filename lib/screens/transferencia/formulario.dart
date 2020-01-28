1e623ef7708ec3285ee4abdf77f7ef3497e6095b
Vinicius <vmsabc@gmail.com>
2020-01-28 09:30:32 -0300

    Insert image in champion details page

--- lib/screens/transferencia/formulario.dart
+++ lib/screens/transferencia/formulario.dart
@@ -1,14 +1,13 @@
-import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:paladins_stats_app/components/editor.dart';
 import '../../models/champion.dart';
 
 class FormularioTransferencia extends StatefulWidget {
   final Champion champion;
 
-  const FormularioTransferencia({Key key, this.champion}) : super(key: key);
+  const FormularioTransferencia({Key key, @required this.champion})
+      : super(key: key);
 
   @override
   State<StatefulWidget> createState() {
@@ -17,46 +16,40 @@ class FormularioTransferencia extends StatefulWidget {
 }
 
 class FormularioTransferenciaState extends State<FormularioTransferencia> {
-  final TextEditingController _controladorCampoNumeroConta =
-      TextEditingController();
-  final TextEditingController _controladorCampoValor = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.champion.feName),
-      ),
-      body: FutureBuilder(
-        future: fetchPost(),
-        builder: (context, snapshot){
-          return SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                Editor(
-                    controlador: _controladorCampoNumeroConta,
-                    rotulo: 'Numero da conta',
-                    dica: '000'),
-                Editor(
-                    controlador: _controladorCampoValor,
-                    rotulo: 'Valor',
-                    dica: '0.00',
-                    icone: Icons.monetization_on),
-              ],
-            ),
-          );
-        },
-      )
-    );
+        appBar: AppBar(
+          title: Text(widget.champion.feName),
+        ),
+        body: FutureBuilder(
+          future: fetchPost(widget.champion.feName),
+          builder: (context, snapshot) {
+            debugPrint('oloco');
+            final champion = json.decode(snapshot.data.toString());
+            return SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  Image.network(
+                      'https://web2.hirez.com/paladins/champion-headers/' +
+                          champion[0]['slug'] +
+                          '.png'),
+                ],
+              ),
+            );
+          },
+        ));
   }
-  fetchPost() async {
-    final response =
-    await http.get('https://cms.paladins.com/wp-json/wp/v2/champions?slug=dredge&lang_id=1');
+
+  fetchPost(championName) async {
+    final response = await http.get(
+        'https://cms.paladins.com/wp-json/wp/v2/champions?slug=' +
+            championName.replaceAll(' ', '-').toLowerCase() +
+            '&lang_id=1');
 
     if (response.statusCode == 200) {
       // If the call to the server was successful, parse the JSON
-      debugPrint(response.body);
-      return null;
+      return response.body;
     } else {
       // If that call was not successful, throw an error.
       throw Exception('Failed to load post');
